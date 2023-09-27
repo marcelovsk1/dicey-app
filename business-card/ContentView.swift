4dd518f361b5c12859b20c0e368e273eda74b67d
Marcelo Amaral Alves <marceloaa@live.com>
2023-09-27 13:35:31 -0400

    Initial Commit

---
+++ business-card/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  business-card
+//
+//  Created by Marcelo Amaral Alves on 2023-09-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ business-card/business_cardApp.swift
@@ -0,0 +1,17 @@
+//
+//  business_cardApp.swift
+//  business-card
+//
+//  Created by Marcelo Amaral Alves on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct business_cardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
